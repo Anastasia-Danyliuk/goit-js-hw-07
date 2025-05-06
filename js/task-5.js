ba0c3231b1c7f82c4ebfbbf807998a2d2db8d476
bexots64 <bexots64@gmail.com>
2025-05-06 22:02:39 +0300

    Add homework 12

--- js/task-5.js
+++ js/task-5.js
@@ -1,12 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
 
-const background = document.querySelector('.color');
+const background = document.querySelector('body');
+const color = document.querySelector('.color');
 const button = document.querySelector('.change-color');
 
-button.addEventListener('click',() => {
-   background.style.background = getRandomHexColor();
+button.addEventListener('click', () => {
+    const backgroundColor = getRandomHexColor();
+    background.style.background = backgroundColor;
+    color.textContent = backgroundColor;
 })
